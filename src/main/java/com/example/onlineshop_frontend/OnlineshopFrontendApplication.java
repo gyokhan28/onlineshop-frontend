4abbe1ff02122923f5a3da25df0759c5b8e57df3
Gyokhan <gyokhan28@gmail.com>
2024-07-16 12:08:27 +0300

    Initial commit

---
+++ src/main/java/com/example/onlineshop_frontend/OnlineshopFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineshop_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlineshop_frontend/OnlineshopFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshop_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
