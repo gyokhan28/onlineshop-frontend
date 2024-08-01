b3795944083e38cc97335d78a34a8ce1b8340e82
berki0 <107307903+berki0@users.noreply.github.com>
2024-08-01 19:37:17 +0300

    add Type of Product

--- src/main/java/com/example/onlineshop_frontend/dto/ProductResponseDto.java
+++ src/main/java/com/example/onlineshop_frontend/dto/ProductResponseDto.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 public class ProductResponseDto {
   private Long id;
   private String name;
+  private String productType;
   private BigDecimal price;
   private int quantity;
   private String imageLocation;
